798eb6423216888981786f6271d4850d557543af
Venkatesh Paruchuri <venkatesh.paruchuri@zaggle.in>
2019-03-08 15:32:03 +0530

    first commit

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -31,19 +31,19 @@ const AppWrapper = styled.div`
 export default function App() {
   return (
     <AppWrapper>
-      <Helmet
+      {/* <Helmet
         titleTemplate="%s - React.js Boilerplate"
         defaultTitle="React.js Boilerplate"
       >
         <meta name="description" content="A React.js Boilerplate application" />
       </Helmet>
-      <Header />
+      <Header /> */}
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/features" component={FeaturePage} />
         <Route path="" component={NotFoundPage} />
       </Switch>
-      <Footer />
+      {/* <Footer /> */}
       <GlobalStyle />
     </AppWrapper>
   );
