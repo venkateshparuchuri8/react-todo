792e92d474bb7bc8262ecdd25afd0c961f6940a4
Venkatesh Paruchuri <venkatesh.paruchuri@zaggle.in>
2019-03-15 11:41:54 +0530

    changes for home page

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -6,8 +6,6 @@
 
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-// import Modal from 'react-modal';
-// import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { createStructuredSelector } from 'reselect';
@@ -19,35 +17,12 @@ import {
   makeSelectLoading,
   makeSelectError,
 } from 'containers/App/selectors';
-// import H2 from 'components/H2';
-// import ReposList from 'components/ReposList';
-// import AtPrefix from './AtPrefix';
-// import CenteredSection from './CenteredSection';
-// import Form from './Form';
-// import Input from './Input';
-// import Section from './Section';
-// import messages from './messages';
 import { Modal, Card, Upload, Button, Icon } from 'antd';
 import { loadRepos } from '../App/actions';
 import { changeUsername } from './actions';
 import { makeSelectUsername } from './selectors';
 import reducer from './reducer';
 import saga from './saga';
-
-// import Modal from '../../components/Modal';
-// import Button from '../../components/Button';
-// import Input from '../../components/Input';
-/* eslint-disable react/prefer-stateless-function */
-// const customStyles = {
-//   content: {
-//     top: '50%',
-//     left: '50%',
-//     right: 'auto',
-//     bottom: 'auto',
-//     marginRight: '-50%',
-//     transform: 'translate(-50%, -50%)',
-//   },
-// };
 
 class HomePage extends Component {
   constructor(props) {
@@ -90,75 +65,10 @@ class HomePage extends Component {
     );
   }
 
-  renderModalBody() {
-    return (
-      <div className="model-content">
-        <h2>sdasd</h2>
-        <p>sdasdasd sds adas das das dasd </p>
-        <div className="top">
-          <input type="text" />
-          <button type="submit">Choose unit procedure</button>
-        </div>
-        <div className="box">
-          <div className="header">
-            <strong>operations</strong>
-            <label id="#bb">
-              Choose file
-              <input type="file" id="File" size="60" />
-            </label>
-          </div>
-          <span>
-            one
-            <p className="close" /> <br />
-            one <br />
-            one <br />
-            one <br />
-            one one one one one one one one one one one one one one one one one
-            one one <br />
-          </span>
-        </div>
-        <div className="box noborder-left">
-          <div className="header">
-            <strong>operations</strong>
-            <label id="#bb">
-              Choose file
-              <input type="file" id="File" size="60" />
-            </label>
-          </div>
-          <span>
-            one <br />
-            one <br />
-            one <br />
-            one <br />
-            one <br />
-            one <br />
-            one <br />
-            one <br />
-            one <br />
-            one <br />
-          </span>
-        </div>
-        <br clear=" all" />
-        <a href="/#">Cancel</a>
-        <button type="submit" className="btn-blue">
-          Import
-        </button>
-      </div>
-    );
-  }
-
   render() {
     const { showModal } = this.state;
     return (
       <div>
-        {/* <Modal
-          isOpen={showModal}
-          onRequestClose={this.handleModal}
-          style={customStyles}
-          contentLabel="Example Modal"
-        >
-          {this.renderModalBody()}
-        </Modal> */}
         <div>
           <Button type="primary" onClick={this.handleModal}>
             Open Modal
@@ -173,9 +83,6 @@ class HomePage extends Component {
             {this.renderModalContent()}
           </Modal>
         </div>
-        {/* <button type="primary" onClick={this.handleModal}>
-          open Modal
-        </button> */}
       </div>
     );
   }
